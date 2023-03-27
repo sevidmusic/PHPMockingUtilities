f3e70a428c74822aa797a9960c7555de6e039a82
sevidmusic <sevidmusic@gmail.com>
2023-03-27 02:29:26 -0400

    Resolved issue #12.
    Implemented the following new class:
    
    ```
    Darling\PHPMockingUtilities\classes\mock\values\MockValue
    
    ```
    
    Which implements the following interface:
    
    ```
    <?php
    
    namespace Darling\PHPMockingUtilities\interfaces\mock\values;
    
    /**
     * A MockValue can be used to mock a value.
     *
     * @example
     *
     * ```
     * var_dump($mockvalue->value());
     *
     * // example output:
     * bool(true)
     *
     * ```
     */
    interface MockValue
    {
    
        /**
         * Return the value of this MockValue.
         *
         * @return mixed
         *
         * @example
         *
         * ```
         * var_dump($mockvalue->value());
         *
         * // example output:
         * int(88)
         *
         * ```
         *
         */
        public function value(): mixed;
    
    }
    
    ```

---
+++ src/classes/mock/values/MockValue.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Darling\PHPMockingUtilities\classes\mock\values;
+
+use Darling\PHPMockingUtilities\interfaces\mock\values\MockValue as MockValueInterface;
+use \stdClass;
+
+class MockValue implements MockValueInterface
+{
+
+    public function __construct(private mixed $value = null)
+    {
+        if(!isset($this->value)) {
+            $this->value = $this->randomValue();
+        }
+    }
+
+    private function randomValue(): mixed
+    {
+        $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $values = [
+            rand(0, 100),
+            str_shuffle($letters),
+            true,
+            false,
+            [],
+            function(): void {},
+            new stdClass(),
+        ];
+        return $values[array_rand($values)];
+    }
+
+    public function value(): mixed
+    {
+        return $this->value;
+    }
+
+}
+
