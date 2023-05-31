a57daf31fce0072d95552647940a0a32618dcff3
sevidmusic <sevidmusic@gmail.com>
2023-05-31 00:53:30 -0400

    Revised documentatuin

--- tests/mock/abstractions/AbstractImplementationOfInterfaceForClassThatDefinesMethods.php
+++ tests/mock/abstractions/AbstractImplementationOfInterfaceForClassThatDefinesMethods.php
@@ -5,11 +5,10 @@ namespace Darling\PHPMockingUtilities\tests\mock\abstractions;
 use \Darling\PHPMockingUtilities\tests\mock\interfaces\InterfaceForClassThatDefinesMethods;
 
 /**
- * The following classes are defined here for use by
- * the MockClassInstanceTest
+ * The following class is defined here for use by the
+ * MockClassInstanceTest
  *
  */
-
 abstract class AbstractImplementationOfInterfaceForClassThatDefinesMethods implements InterfaceForClassThatDefinesMethods
 {
 }
--- tests/mock/classes/ClassThatDoesNotDefineMethods.php
+++ tests/mock/classes/ClassThatDoesNotDefineMethods.php
@@ -2,6 +2,11 @@
 
 namespace Darling\PHPMockingUtilities\tests\mock\classes;
 
+/**
+ * The following class is defined here for use by the
+ * MockClassInstanceTest
+ *
+ */
 class ClassThatDoesNotDefineMethods
 {
 
