ce47bea1bb58fbaa0ca8a65928b99d9703a02f5f
sevidmusic <sevidmusic@gmail.com>
2023-06-01 02:39:32 -0400

    Code clean up

--- tests/classes/mock/values/MockClassInstanceTest.php
+++ tests/classes/mock/values/MockClassInstanceTest.php
@@ -5,18 +5,8 @@ namespace Darling\PHPMockingUtilities\tests\classes\mock\values;
 use \Darling\PHPMockingUtilities\classes\mock\values\MockClassInstance;
 use \Darling\PHPMockingUtilities\tests\PHPMockingUtilitiesTest;
 use \Darling\PHPMockingUtilities\tests\interfaces\mock\values\MockClassInstanceTestTrait;
-use \Darling\PHPMockingUtilities\tests\mock\abstractions\AbstractImplementationOfInterfaceForClassThatDefinesMethods;
-use \Darling\PHPMockingUtilities\tests\mock\classes\ClassThatDoesNotDefineMethods;
-use \Darling\PHPMockingUtilities\tests\mock\classes\ImplementationOfInterfaceForClassThatDefinesMethods;
-use \Darling\PHPMockingUtilities\tests\mock\interfaces\InterfaceForClassThatDefinesMethods;
 use \Darling\PHPReflectionUtilities\classes\utilities\Reflection;
-use \Darling\PHPReflectionUtilities\interfaces\utilities\Reflection as ReflectionInterface;
 use \Darling\PHPTextTypes\classes\strings\ClassString;
-use \Darling\PHPTextTypes\classes\strings\Name;
-use \Darling\PHPTextTypes\classes\strings\Text;
-use \Darling\PHPTextTypes\classes\strings\UnknownClass;
-use \Darling\PHPTextTypes\interfaces\strings\Name as NameInterface;
-use \Darling\PHPTextTypes\interfaces\strings\Text as TextInterface;
 use \ReflectionClass;
 use \Stringable;
 use \stdClass;
@@ -52,49 +42,5 @@ class MockClassInstanceTest extends PHPMockingUtilitiesTest
         );
     }
 
-    /**
-     * Return a random fully qualified class name, or object instance.
-     *
-     * @return class-string|object
-     *
-     * @example
-     *
-     * ```
-     * var_dump($this->randomClassStringOrObjectInstance);
-     *
-     * // example output:
-     * string(8) "stdClass"
-     *
-     * ```
-     *
-     */
-    public function randomClassStringOrObjectInstance(): string|object
-    {
-        /** @var array<int, class-string|object> $classes */
-        $classes = [
-            parent::randomClassStringOrObjectInstance(),
-            AbstractImplementationOfInterfaceForClassThatDefinesMethods::class,
-            ClassThatDoesNotDefineMethods::class,
-            ImplementationOfInterfaceForClassThatDefinesMethods::class,
-            new ImplementationOfInterfaceForClassThatDefinesMethods(),
-            InterfaceForClassThatDefinesMethods::class,
-            Name::class,
-            NameInterface::class,
-            Text::class,
-            TextInterface::class,
-            new ClassThatDoesNotDefineMethods(),
-            new Name(new Text($this->randomChars())),
-            new Text($this->randomChars()),
-            new stdClass(),
-            stdClass::class,
-            parent::randomClassStringOrObjectInstance(),
-            function(): void {},
-        ];
-        return (
-            empty($classes)
-            ? new stdClass()
-            : $classes[array_rand($classes)]
-        );
-    }
 }
 
--- tests/interfaces/mock/values/MockClassInstanceTestTrait.php
+++ tests/interfaces/mock/values/MockClassInstanceTestTrait.php
@@ -2,13 +2,26 @@
 
 namespace Darling\PHPMockingUtilities\tests\interfaces\mock\values;
 
+use \Closure;
+use \Darling\PHPMockingUtilities\classes\mock\values\MockClassInstance as MockClassInstanceFOO;
 use \Darling\PHPMockingUtilities\interfaces\mock\values\MockClassInstance;
+use \Darling\PHPMockingUtilities\tests\PHPMockingUtilitiesTest;
+use \Darling\PHPMockingUtilities\tests\interfaces\mock\values\MockClassInstanceTestTrait;
+use \Darling\PHPMockingUtilities\tests\mock\abstractions\AbstractImplementationOfInterfaceForClassThatDefinesMethods;
+use \Darling\PHPMockingUtilities\tests\mock\classes\ClassThatDoesNotDefineMethods;
+use \Darling\PHPMockingUtilities\tests\mock\classes\ImplementationOfInterfaceForClassThatDefinesMethods;
+use \Darling\PHPMockingUtilities\tests\mock\interfaces\InterfaceForClassThatDefinesMethods;
 use \Darling\PHPReflectionUtilities\classes\utilities\Reflection as ReflectionInstance;
 use \Darling\PHPReflectionUtilities\interfaces\utilities\Reflection;
+use \Darling\PHPTextTypes\classes\strings\ClassString;
+use \Darling\PHPTextTypes\classes\strings\Name;
+use \Darling\PHPTextTypes\classes\strings\Text;
 use \Darling\PHPTextTypes\classes\strings\UnknownClass;
+use \Darling\PHPTextTypes\interfaces\strings\Name as NameInterface;
+use \Darling\PHPTextTypes\interfaces\strings\Text as TextInterface;
 use \ReflectionClass;
 use \Stringable;
-use \Closure;
+use \stdClass;
 
 /**
  * The MockClassInstanceTestTrait defines common tests for
@@ -618,5 +631,49 @@ trait MockClassInstanceTestTrait
         return $methodNames[array_rand($methodNames)];
     }
 
+    /**
+     * Return a random fully qualified class name, or object instance.
+     *
+     * @return class-string|object
+     *
+     * @example
+     *
+     * ```
+     * var_dump($this->randomClassStringOrObjectInstance);
+     *
+     * // example output:
+     * string(8) "stdClass"
+     *
+     * ```
+     *
+     */
+    protected function randomClassStringOrObjectInstance(): string|object
+    {
+        /** @var array<int, class-string|object> $classes */
+        $classes = [
+            parent::randomClassStringOrObjectInstance(),
+            AbstractImplementationOfInterfaceForClassThatDefinesMethods::class,
+            ClassThatDoesNotDefineMethods::class,
+            ImplementationOfInterfaceForClassThatDefinesMethods::class,
+            new ImplementationOfInterfaceForClassThatDefinesMethods(),
+            InterfaceForClassThatDefinesMethods::class,
+            Name::class,
+            NameInterface::class,
+            Text::class,
+            TextInterface::class,
+            new ClassThatDoesNotDefineMethods(),
+            new Name(new Text($this->randomChars())),
+            new Text($this->randomChars()),
+            new stdClass(),
+            stdClass::class,
+            parent::randomClassStringOrObjectInstance(),
+            function(): void {},
+        ];
+        return (
+            empty($classes)
+            ? new stdClass()
+            : $classes[array_rand($classes)]
+        );
+    }
 }
 
